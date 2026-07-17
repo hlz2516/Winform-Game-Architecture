f8eeb895cb7c538768f013040e14ba163fb38649
hlz2516 <714251494@qq.com>
2026-07-17 16:53:43 +0800

    因winform 伪透明问题，转向wpf框架

--- 贪吃蛇/Form1.cs
+++ 贪吃蛇/Form1.cs
@@ -68,5 +68,10 @@ namespace 贪吃蛇
             }
             return base.ProcessDialogKey(keyData);
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
